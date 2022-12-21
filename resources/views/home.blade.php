1e60fd8faa4bb843bc5bc16116460ffa58ea93f2
Elena <elenarjona@gmail.com>
2022-12-21 15:55:56 +0100

    Read

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    @foreach ($adoptions as $adoption)
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{ $adoption->name }}</h5>
+                <h5 class="card-title">{{ $adoption->spaces }} Abrazos</h5>
+                <h5 class="card-title">{{ $adoption->datetime }}</h5>
+                <p class="card-text">{{ $adoption->description }}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
-    </div>
-</div>
+    @endforeach
+
 @endsection
