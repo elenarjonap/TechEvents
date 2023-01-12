9a6251a439a1c045e88669578f93421f7ff5c580
birdsinyourgarden <ruizpaloma9@gmail.com>
2023-01-12 10:07:51 +0100

    style home

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="mainFooter p-4 mt-4">
     <div class="container d-flex justify-content-between align-items-center">
-        <p class="footerInfo mb-0">©️ Las Notas 2022</p>
+        <p class="footerInfo mb-0">©️ Las Notas 2023</p>
         <div class="footerSocialMedia">
             <a href="#"><img src="" alt="Las Notas Logo"></a>
             <a href="#"><img src="" alt="Abrazarnos Instagram"></a>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,14 +11,14 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.bunny.net/css?family=Open-Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&display=swap" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
     <!-- CSS only -->
-    <link rel="stylesheet" href="/css/app.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/app.css"> 
 </head>
 <body>
     <div id="app">
