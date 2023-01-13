a7985856d0bf94c6873a0629f5f3d3d25bfdb3b8
Elena <elenarjona@gmail.com>
2023-01-13 12:29:01 +0100

    images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,13 @@
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="https://img.freepik.com/foto-gratis/adorable-gatito-blanco-negro-pared-monocromatica-detras-ella_23-2148955167.jpg?w=1380&t=st=1673349242~exp=1673349842~hmac=dfd149c1102a8b8c3f24ab38dc95166be240f05b3cdf920c25d6d357f2b57add" class="d-block w-100" alt="...">
+            <img src="https://res.cloudinary.com/dog5ljnve/image/upload/v1673605060/LasNotas/gato_zjk7lp.png" class="d-block w-100" alt="fotografía de gato">
         </div>
     <div class="carousel-item">
-        <img src="https://img.freepik.com/foto-gratis/hombre-barbudo-acariciando-lindo-pajaro_23-2148740489.jpg?w=1480&t=st=1673348879~exp=1673349479~hmac=7c72174717e4a379fbd0482ef5c79c3cf8e3b2a565703a5905dc68872b55f879
-" class="d-block w-100" alt="...">
+        <img src="https://res.cloudinary.com/dog5ljnve/image/upload/v1673604839/LasNotas/loro_ozajwd.png" class="d-block w-100" alt="fotografía de loro">
     </div>
     <div class="carousel-item">
-        <img src="" class="d-block w-100" alt="...">
+        <img src="https://res.cloudinary.com/dog5ljnve/image/upload/v1673606274/LasNotas/perro-min_vjcpka.png" class="d-block w-100" alt="fotografía de perro">
     </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
