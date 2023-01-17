4f049fcd74c554c080c96e933fe62a9d9203b914
Elena <elenarjona@gmail.com>
2023-01-17 14:19:00 +0100

    button

--- resources/views/showAdoption.blade.php
+++ resources/views/showAdoption.blade.php
@@ -9,8 +9,8 @@
 				<h3 class="text-center mb-4"><span class="h2">{{ $adoption->spaces }} Abrazos</span> · {{ $adoption->datetime }}</h3>
 				<h2 class="">{{ $adoption->description }}</h2>
 				<div class="d-flex flex-column">
-					<a href="#" class="btn btn-primary mt-4 mb-4 fs-2">Abrázame</a>
-					<a href="{{route('unscribe', $adoption->id)}}" class="btn text-end mb-2"><img src="/images/cancel.png"></a>
+					<a href="{{ route('inscribe', $adoption->id) }}" class="btn btn-primary mt-4 mb-4 fs-2">Abrázame</a>
+					<a href="{{ route('unscribe', $adoption->id) }}" class="btn btn-danger mt-4 mb-4 fs-2">Detener inscripción</a>
 				</div>
 			</div>
 		</div>
